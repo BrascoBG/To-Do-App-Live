ddfdd30209d9855857ff274a864b169d2ea8f7ef
Ivo Krastev <ivo.krustev@yahoo.com>
2020-03-31 14:40:16 +0300

    Updates

---
+++ precache-manifest.365ee5ebd5bc9c01cff8b95e912f6832.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0561a5431909df5a4b8d0b313bfb0a3",
+    "url": "/To-Do-App-Live/index.html"
+  },
+  {
+    "revision": "6a1aa4b166248de4856d",
+    "url": "/To-Do-App-Live/static/css/main.597432e8.chunk.css"
+  },
+  {
+    "revision": "ad6f5f7535de164345b5",
+    "url": "/To-Do-App-Live/static/js/2.27ea6b9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-App-Live/static/js/2.27ea6b9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a1aa4b166248de4856d",
+    "url": "/To-Do-App-Live/static/js/main.92256d06.chunk.js"
+  },
+  {
+    "revision": "537e67b11c88f931e091",
+    "url": "/To-Do-App-Live/static/js/runtime-main.54441c0d.js"
+  }
+]);
